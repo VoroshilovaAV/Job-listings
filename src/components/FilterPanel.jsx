f58c3987685c2fafd2a4f93310e23ec70f29a2e9
VoroshilovaAV <Voroshilova.AV@yandex.ru>
2022-11-20 13:38:15 +0400

    feat: add logic to remove and clear filters

--- src/components/FilterPanel.jsx
+++ src/components/FilterPanel.jsx
@@ -1,19 +1,41 @@
+import { useSelector, useDispatch } from 'react-redux';
+
 import { Badge } from 'UI/Badge';
 import { Card } from 'UI/Card';
 import { Stack } from 'UI/Stack';
 
+import { selectFilters } from 'store/filters/filter-selectors'
+import { removeFilter, clearFilter } from 'store/filters/filter-actions'
 
 const FilterPanel = () => {
+  const dispatch = useDispatch();
+  const currentFilters = useSelector(selectFilters);
+
+  if (currentFilters.length === 0){
+    return null;
+  }
+
   return (
     <Card className="filter-panel">
       <div className="filter-panel-wrapper">
-        <Stack>
-          <Badge variant="clearable">Frontend</Badge>
-          <Badge variant="clearable">Backend</Badge>
-          <Badge variant="clearable">React</Badge>
+        <Stack>{
+          currentFilters.map(filter => (
+            <Badge 
+              key={filter}
+              variant="clearable"
+              onClear={() => dispatch(removeFilter(filter))}
+            >
+              {filter}
+            </Badge>
+          ))
+        }
         </Stack>
-
-        <button className='link'>Clear</button>
+        <button 
+          className='link'
+          onClick={()=> dispatch(clearFilter)}
+        >
+          Clear
+        </button>
       </div>
     </Card>
   )
--- src/components/JobList.jsx
+++ src/components/JobList.jsx
@@ -1,11 +1,15 @@
 import { useSelector, useDispatch } from 'react-redux'
 import { JobPosition } from './JobPosition';
-import { selectAllPositions } from 'store/positions/position-selectors'
+import { selectVisiblePositions } from 'store/positions/position-selectors';
+import { selectFilters } from 'store/filters/filter-selectors'
 
 import { addFilter } from 'store/filters/filter-actions'
 
 const JobList = () => {
-  const positions = useSelector(selectAllPositions);
+  const positions = useSelector((state) => {
+    selectVisiblePositions(state, currentFilters);
+  });
+  const currentFilters = useSelector(selectFilters);
   const dispatch = useDispatch();
 
   const handleAddFilter = (filter) => {
